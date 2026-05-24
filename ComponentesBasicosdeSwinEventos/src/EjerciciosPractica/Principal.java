793a84a40d79e7873bfcf02daf9719f69951c3f9
eliandradeplu3006-ctrl <eliandradeplu3006@gmail.com>
2026-05-24 13:11:40 -0500

    Componentes Swing eventos

---
+++ ComponentesBasicosdeSwinEventos/src/EjerciciosPractica/Principal.java
@@ -0,0 +1,10 @@
+package EjerciciosPractica;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FormularioPrincipal_Grupo8 menu = new FormularioPrincipal_Grupo8();
+        menu.setVisible(true);
+    }
+    
+}
